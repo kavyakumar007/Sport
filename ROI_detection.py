b0aa0ce8f956454689cc42b10394c335ad7657c7
Kavya Kumar <f20221625@pilani.bits-pilani.ac.in>
2026-05-25 14:01:58 +0530

    Update ROI_detection.py

--- ROI_detection.py
+++ ROI_detection.py
@@ -1,3 +1,7 @@
+import cv2
+import numpy as np
+import argparse
+import sys
 # ──────────────────────────────────────────────────────────
 # CONFIG  (edit these after running with --roi)
 # ──────────────────────────────────────────────────────────
